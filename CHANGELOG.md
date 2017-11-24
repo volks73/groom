b0e33a46a4c6e900db7779e7250104bc42d2e81b
Christopher Field <cfield2@gmail.com>
2017-11-24 11:08:11 -0500

    Add licensing information

---
+++ CHANGELOG.md
@@ -0,0 +1,15 @@
+# Groom: Change Log
+
+All notable changes to this project will be documented in this file, which is written in plain text (ASCII) [Github Flavored Markdown (GFM)](https://help.github.com/articles/github-flavored-markdown/) lightweight markup language. This project adheres to [Semantic Versioning](http://semver.org).
+
+## [Unreleased][unreleased]
+
+### Added 
+
+- The `MAPPING` argument.
+- The `-i,--input` command line option.
+- The `-o,--output` command line option.
+- The `-h,--help` command line flag, which displays the help text for the application.
+- The `-V,--version` command line flag, which displays the current version for the application.
+- The `-v,--verbose` command line flag, which increases the logging statements while the application is executed.
+
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+// Copyright (C) 2017 Christopher R. Field.
+//
+// This file is part of Groom.
+//
+// Groom is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// Groom is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with Groom.  If not, see <http://www.gnu.org/licenses/>.
+
 //! `groom` - A command line utility for processing [mustache](https://mustache.github.io/)
 //! templates written in rust.
 
@@ -84,3 +101,4 @@ fn main() {
         }
     }
 }
+
